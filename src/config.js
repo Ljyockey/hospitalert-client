29d6aa3ae51824353358df356ff91c91dc289f95
ljyockey <linyockey@gmail.com>
2017-07-20 14:52:24 -0700

    Netlify env varibales

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
-  API_BASE_URL: 'http://localhost:8080' || 'https://hospitalert-api.herokuapp.com',
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
+    "http://localhost:8080",
   APP_ID: '126180944566416'
 };
